3c2d2d934933ee748637ee26d4fcbe5903d2a77d
JulieRapson <rapson_julie@columbusstate.edu>
2023-03-14 21:44:24 -0400

    Full convert unit project commit

---
+++ convert_units/Form1.Designer.vb
@@ -0,0 +1,200 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.picBuilding = New System.Windows.Forms.PictureBox()
+        Me.lblAppTitle = New System.Windows.Forms.Label()
+        Me.lblInstructions = New System.Windows.Forms.Label()
+        Me.txtEnteredNum = New System.Windows.Forms.TextBox()
+        Me.rdInchesToMeters = New System.Windows.Forms.RadioButton()
+        Me.rdMetersToInches = New System.Windows.Forms.RadioButton()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.btnConvert = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btExit = New System.Windows.Forms.Button()
+        Me.lblResults = New System.Windows.Forms.Label()
+        CType(Me.picBuilding, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'picBuilding
+        '
+        Me.picBuilding.Image = Global.convert_units.My.Resources.Resources.building
+        Me.picBuilding.Location = New System.Drawing.Point(3, 2)
+        Me.picBuilding.Name = "picBuilding"
+        Me.picBuilding.Size = New System.Drawing.Size(274, 186)
+        Me.picBuilding.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.picBuilding.TabIndex = 0
+        Me.picBuilding.TabStop = False
+        '
+        'lblAppTitle
+        '
+        Me.lblAppTitle.AutoSize = True
+        Me.lblAppTitle.Font = New System.Drawing.Font("MS Reference Sans Serif", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblAppTitle.Location = New System.Drawing.Point(345, 33)
+        Me.lblAppTitle.Name = "lblAppTitle"
+        Me.lblAppTitle.Size = New System.Drawing.Size(198, 29)
+        Me.lblAppTitle.TabIndex = 1
+        Me.lblAppTitle.Text = "Converter App"
+        '
+        'lblInstructions
+        '
+        Me.lblInstructions.AutoSize = True
+        Me.lblInstructions.Font = New System.Drawing.Font("MS Reference Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblInstructions.Location = New System.Drawing.Point(342, 107)
+        Me.lblInstructions.Name = "lblInstructions"
+        Me.lblInstructions.Size = New System.Drawing.Size(205, 48)
+        Me.lblInstructions.TabIndex = 2
+        Me.lblInstructions.Text = "Enter a value and " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "choose conversion" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        '
+        'txtEnteredNum
+        '
+        Me.txtEnteredNum.Font = New System.Drawing.Font("MS Reference Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtEnteredNum.Location = New System.Drawing.Point(618, 124)
+        Me.txtEnteredNum.Name = "txtEnteredNum"
+        Me.txtEnteredNum.Size = New System.Drawing.Size(100, 31)
+        Me.txtEnteredNum.TabIndex = 3
+        '
+        'rdInchesToMeters
+        '
+        Me.rdInchesToMeters.AutoSize = True
+        Me.rdInchesToMeters.Font = New System.Drawing.Font("MS Reference Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.rdInchesToMeters.Location = New System.Drawing.Point(29, 38)
+        Me.rdInchesToMeters.Name = "rdInchesToMeters"
+        Me.rdInchesToMeters.Size = New System.Drawing.Size(181, 24)
+        Me.rdInchesToMeters.TabIndex = 4
+        Me.rdInchesToMeters.TabStop = True
+        Me.rdInchesToMeters.Text = "Inches to Meters"
+        Me.rdInchesToMeters.UseVisualStyleBackColor = True
+        '
+        'rdMetersToInches
+        '
+        Me.rdMetersToInches.AutoSize = True
+        Me.rdMetersToInches.Font = New System.Drawing.Font("MS Reference Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.rdMetersToInches.Location = New System.Drawing.Point(29, 68)
+        Me.rdMetersToInches.Name = "rdMetersToInches"
+        Me.rdMetersToInches.Size = New System.Drawing.Size(181, 24)
+        Me.rdMetersToInches.TabIndex = 5
+        Me.rdMetersToInches.TabStop = True
+        Me.rdMetersToInches.Text = "Meters to Inches"
+        Me.rdMetersToInches.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.BackColor = System.Drawing.Color.DimGray
+        Me.GroupBox1.Controls.Add(Me.rdInchesToMeters)
+        Me.GroupBox1.Controls.Add(Me.rdMetersToInches)
+        Me.GroupBox1.Font = New System.Drawing.Font("MS Reference Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox1.ForeColor = System.Drawing.Color.White
+        Me.GroupBox1.Location = New System.Drawing.Point(372, 203)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(318, 122)
+        Me.GroupBox1.TabIndex = 6
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Convert Measurement"
+        '
+        'btnConvert
+        '
+        Me.btnConvert.BackColor = System.Drawing.Color.MediumPurple
+        Me.btnConvert.Font = New System.Drawing.Font("MS Reference Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnConvert.ForeColor = System.Drawing.Color.White
+        Me.btnConvert.Location = New System.Drawing.Point(70, 415)
+        Me.btnConvert.Name = "btnConvert"
+        Me.btnConvert.Size = New System.Drawing.Size(169, 46)
+        Me.btnConvert.TabIndex = 7
+        Me.btnConvert.Text = "Convert"
+        Me.btnConvert.UseVisualStyleBackColor = False
+        '
+        'btnClear
+        '
+        Me.btnClear.BackColor = System.Drawing.Color.MediumPurple
+        Me.btnClear.Font = New System.Drawing.Font("MS Reference Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnClear.ForeColor = System.Drawing.Color.White
+        Me.btnClear.Location = New System.Drawing.Point(344, 415)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(169, 46)
+        Me.btnClear.TabIndex = 8
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = False
+        '
+        'btExit
+        '
+        Me.btExit.BackColor = System.Drawing.Color.MediumPurple
+        Me.btExit.Font = New System.Drawing.Font("MS Reference Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btExit.ForeColor = System.Drawing.Color.White
+        Me.btExit.Location = New System.Drawing.Point(618, 415)
+        Me.btExit.Name = "btExit"
+        Me.btExit.Size = New System.Drawing.Size(169, 46)
+        Me.btExit.TabIndex = 9
+        Me.btExit.Text = "Exit"
+        Me.btExit.UseVisualStyleBackColor = False
+        '
+        'lblResults
+        '
+        Me.lblResults.AutoSize = True
+        Me.lblResults.Font = New System.Drawing.Font("MS Reference Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblResults.Location = New System.Drawing.Point(346, 358)
+        Me.lblResults.Name = "lblResults"
+        Me.lblResults.Size = New System.Drawing.Size(79, 24)
+        Me.lblResults.TabIndex = 10
+        Me.lblResults.Text = "Label1"
+        Me.lblResults.Visible = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(834, 507)
+        Me.Controls.Add(Me.lblResults)
+        Me.Controls.Add(Me.btExit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnConvert)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.txtEnteredNum)
+        Me.Controls.Add(Me.lblInstructions)
+        Me.Controls.Add(Me.lblAppTitle)
+        Me.Controls.Add(Me.picBuilding)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "Building Plans Conversion"
+        CType(Me.picBuilding, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents picBuilding As PictureBox
+    Friend WithEvents lblAppTitle As Label
+    Friend WithEvents lblInstructions As Label
+    Friend WithEvents txtEnteredNum As TextBox
+    Friend WithEvents rdInchesToMeters As RadioButton
+    Friend WithEvents rdMetersToInches As RadioButton
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents btnConvert As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btExit As Button
+    Friend WithEvents lblResults As Label
+End Class
---
+++ convert_units/Form1.vb
@@ -0,0 +1,104 @@
+﻿'Author: Julie Rapson
+'Date: 3/14/2023
+'Program Description: This program is a basic unit converter.  You will enter a value, choose a selection and then
+'click convert.  Your selection options are Inches to Meters or Meters to Inches.  The program will do the calculation and
+'return the result for you. You can clear and start again. You can also exit the program.
+
+Public Class Form1
+    Private Sub RadioButton1_CheckedChanged(sender As Object, e As EventArgs) Handles rdInchesToMeters.CheckedChanged
+
+    End Sub
+
+    Private Sub btExit_Click(sender As Object, e As EventArgs) Handles btExit.Click
+        'Exit the program when the button is clicked.
+        Close()
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        'Setting the default radio button selection to Inches to meters. Clearing out the results area and the text box.
+        'Making the text box the focus area.
+        rdInchesToMeters.Checked = True
+        txtEnteredNum.Text = ""
+        lblResults.Text = ""
+        txtEnteredNum.Focus()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'Setting the default radio button selection to Inches to meters. Clearing out the results area and the text box.
+        'Making the text box the focus area.
+        rdInchesToMeters.Checked = True
+        txtEnteredNum.Text = ""
+        lblResults.Text = ""
+        txtEnteredNum.Focus()
+
+    End Sub
+
+    Private Sub btnConvert_Click(sender As Object, e As EventArgs) Handles btnConvert.Click
+        'Declaring needed variables below. I had to do a check for numeric as well as creating
+        'a variable for the result set.  I also set the values for the conversion calculations.
+        Dim checkNum = IsNumeric(txtEnteredNum.Text)
+        Dim conversionResult As Decimal
+        Dim myNumtoconvert As String
+        Dim decMeter As Decimal = 0.0254D
+        Dim decInch As Decimal = 39.3701D
+        Dim unitOfMeasure As String = "meters"
+        Dim unitOfMeasure2 As String = " inches"
+
+        'Dim myNumtoconvert = Convert.ToDecimal(txtEnteredNum.Text) 'Error happens here,but if I remove this, it doesnt calculate my values correctly
+
+        'First thing to do is check if what was entered was an actual number, if yes convert to Decimal
+        If IsNumeric(txtEnteredNum.Text) Then
+            myNumtoconvert = Convert.ToDecimal(txtEnteredNum.Text)
+            If myNumtoconvert > 0 Then
+                'This is making sure its positive, if it is, do the math work!
+                'Inches to Meters selection
+                If rdInchesToMeters.Checked Then
+                    conversionResult = myNumtoconvert * decMeter
+                    unitOfMeasure = "inches "
+                    unitOfMeasure2 = " meters"
+                    'Meters to Inches selection
+                ElseIf rdMetersToInches.Checked Then
+                    conversionResult = myNumtoconvert * decInch
+                    unitOfMeasure = "meters"
+                    unitOfMeasure2 = " inches"
+                End If
+                'Take the results and print to the screen
+                lblResults.Text = myNumtoconvert.ToString() + " " + unitOfMeasure + " is " + conversionResult.ToString() + unitOfMeasure2
+                lblResults.Visible = True
+            Else
+                MsgBox("Please enter a positive value only.")
+                lblResults.Text = ""
+            End If
+            'You did not pass the first test, so you automatically come here and get an error message
+        Else
+            MsgBox("Please only enter a valid number for your group.")
+            lblResults.Text = ""
+        End If
+        'I left my old code below for my personal learning experience. I loved the analogy with good person, bad person! :) 
+
+        'If String.IsNullOrEmpty(txtEnteredNum.Text) Then
+        'MsgBox("Please enter a value in order to proceed.")
+        'lblResults.Visible = False
+        'ElseIf checkNum = False Then
+        'MsgBox("Please only enter a valid number for your group.")
+        'lblResults.Visible = False
+        'ElseIf txtEnteredNum.Text < 1 Then
+        'MsgBox("Please enter a positive value only.")
+        'lblResults.Visible = False
+        'ElseIf rdInchesToMeters.Checked = True Then
+        'conversionResult = Convert.ToDecimal(txtEnteredNum.Text) * decMeter
+        'unitOfMeasure = "inches "
+        'unitOfMeasure2 = " meters"
+        'lblResults.Text = myNumtoconvert.ToString() + " inches is " + conversionResult.ToString() + " meters."
+        ' ElseIf rdMetersToInches.Checked = True Then
+        'conversionResult = Convert.ToInt32(txtEnteredNum.Text) * decInch
+        'unitOfMeasure = "meters"
+        'unitOfMeasure2 = " inches"
+        'lblResults.Text = myNumtoconvert.ToString() + " meters is " + conversionResult.ToString() + " inches." 
+        'End If
+        'Make label visible and show results of calculations
+        'lblResults.Text = myNumtoconvert.ToString() + " " + unitOfMeasure + " is " + conversionResult.ToString() + unitOfMeasure2
+        'lblResults.Visible = True
+
+    End Sub
+End Class
